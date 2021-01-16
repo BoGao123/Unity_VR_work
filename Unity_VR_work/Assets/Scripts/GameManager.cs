955c6deed4ef60d848443737908187d8162e24c4
BoGao123 <71680028+BoGao123@users.noreply.github.com>
2021-01-17 04:29:46 +0800

    剩發佈測試

---
+++ Unity_VR_work/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestarGame()
+    {
+        SceneManager.LoadScene("期末");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Unity_VR_work/Assets/Scripts/ScoreManager.cs
+++ Unity_VR_work/Assets/Scripts/ScoreManager.cs
@@ -1,8 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-   
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "糖果" && other.transform.position.y > 2f)
+        {
+            AddScore();
+        }
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 2;
+        }
+    }
+
+
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "分數" + score;
+        aud.PlayOneShot(soundIn, Random.Range(3f, 5f));
+    }
 
 
 
